f59ffbec1a845fcb21d9554607100c01eee4a36f
Paloma Fanticelli <fanticelli.dev@gmail.com>
2025-06-02 16:50:52 +0000

    Conclusão do codigo.

--- superTrunfo.c
+++ superTrunfo.c
@@ -37,13 +37,30 @@ struct dadosSuperPoder {
     float superCarta1, superCarta2;
 };
 
+struct ResultadoRodada {
+    float somaCarta1;
+    float somaCarta2;
+};
+
+//Receber valor dos atributos para soma.
+float obterValorAtributo(const struct Carta* carta, int tipoAtributo) {
+    switch (tipoAtributo) {
+        case 1: return (float)carta->populacao;
+        case 2: return carta->area;
+        case 3: return carta->PIB;
+        case 4: return (float)carta->pontosTuristicos;
+        case 5: return carta->resultados.densidadePopulacional;
+        default: return 0.0f;
+    }
+}
+
 //função para comparação de atributo.
 void compararAtributo(const struct Carta* carta1, const struct Carta* carta2, int tipoAtributo){
     const char* resultadoComparacao;
 
     //exibe o nome do atributo.
     printf("------------------------------------\n");
-    printf("||     %s\n", nomesAtributos[tipoAtributo]);
+    printf("   ------------%s----------\n", nomesAtributos[tipoAtributo]);
     printf("|| %s X %s\n", carta1->cidade, carta2->cidade);
 
     switch (tipoAtributo){
@@ -53,8 +70,8 @@ void compararAtributo(const struct Carta* carta1, const struct Carta* carta2, in
             printf("|| A Carta 1 possui: %lu hab\n", carta1->populacao);
             printf("|| A Carta 2 possui: %lu hab\n", carta2->populacao);
 
-            resultadoComparacao = (carta1->populacao > carta2->populacao) ? "Carta 1 venceu!!" : 
-            (carta2->populacao > carta1->populacao) ? "Carta 2 venceu!!" :
+            resultadoComparacao = (carta1->populacao > carta2->populacao) ? "\nCarta 1 venceu!!" : 
+            (carta2->populacao > carta1->populacao) ? "\nCarta 2 venceu!!" :
             "Empate";
         break;
 
@@ -63,8 +80,8 @@ void compararAtributo(const struct Carta* carta1, const struct Carta* carta2, in
             printf("|| A Carta 1 possui: %.2f km²\n", carta1->area);
             printf("|| A Carta 2 possui: %.2f km²\n", carta2->area);
 
-            resultadoComparacao = (carta1->area > carta2->area) ? "Carta 1 venceu!!" :
-            (carta2->area > carta1->area) ? "Carta 2 venceu!!" :
+            resultadoComparacao = (carta1->area > carta2->area) ? "\nCarta 1 venceu!!" :
+            (carta2->area > carta1->area) ? "\nCarta 2 venceu!!" :
             "Empate";
         break;
 
@@ -73,8 +90,8 @@ void compararAtributo(const struct Carta* carta1, const struct Carta* carta2, in
             printf("|| A Carta 1 possui: %.2f reais\n", carta1->PIB);
             printf("|| A Carta 2 possui: %.2f reais\n", carta2->PIB);
             
-            resultadoComparacao = (carta1->PIB > carta2->PIB) ? "Carta 1 venceu!!" :
-            (carta2->PIB > carta1->PIB) ? "Carta 2 venceu!!" :
+            resultadoComparacao = (carta1->PIB > carta2->PIB) ? "\nCarta 1 venceu!!" :
+            (carta2->PIB > carta1->PIB) ? "\nCarta 2 venceu!!" :
             "Empate";
         break;
 
@@ -83,8 +100,8 @@ void compararAtributo(const struct Carta* carta1, const struct Carta* carta2, in
             printf("|| A Carta 1 possui: %d Pontos Turisticos\n", carta1->pontosTuristicos);
             printf("|| A Carta 2 possui: %d Pontos Turisticos\n", carta2->pontosTuristicos);
 
-            resultadoComparacao = (carta1->pontosTuristicos > carta2->pontosTuristicos) ? "Carta 1 venceu!!" :
-            (carta2->pontosTuristicos > carta1->pontosTuristicos) ? "Carta 2 venceu!!" :
+            resultadoComparacao = (carta1->pontosTuristicos > carta2->pontosTuristicos) ? "\nCarta 1 venceu!!" :
+            (carta2->pontosTuristicos > carta1->pontosTuristicos) ? "\nCarta 2 venceu!!" :
             "Empate";
         break;
 
@@ -93,8 +110,8 @@ void compararAtributo(const struct Carta* carta1, const struct Carta* carta2, in
             printf("|| A Carta 1 possui: %.2f hab/km²\n", carta1->resultados.densidadePopulacional);
             printf("|| A Carta 2 possui: %.2f hab/km²\n", carta2->resultados.densidadePopulacional);
 
-            resultadoComparacao = (carta1->resultados.densidadePopulacional < carta2->resultados.densidadePopulacional) ? "Carta 1 venceu!!" :
-            (carta2->resultados.densidadePopulacional < carta1->resultados.densidadePopulacional) ? "Carta 2 venceu!!" :
+            resultadoComparacao = (carta1->resultados.densidadePopulacional < carta2->resultados.densidadePopulacional) ? "\nCarta 1 venceu!!" :
+            (carta2->resultados.densidadePopulacional < carta1->resultados.densidadePopulacional) ? "\nCarta 2 venceu!!" :
             "Empate";
         break;
 
@@ -102,8 +119,8 @@ void compararAtributo(const struct Carta* carta1, const struct Carta* carta2, in
             printf(" Opção inválida!");
         break;
     }
-    printf("         %s\n", resultadoComparacao);
-    printf("------------------------------------\n");
+    printf("-----------%s------------\n", resultadoComparacao);
+    printf("------------------------------------");
 }
 
 int main() {
@@ -148,7 +165,7 @@ int main() {
 
     printf(" Digite a letra do estado (A-H): ");
     scanf(" %c", &carta2.estado); //lê somente 1 caractere.
-    carta2.estado = toupper(carta1.estado); //converte caractere minusculo em maiusculo.
+    carta2.estado = toupper(carta2.estado); //converte caractere minusculo em maiusculo.
     limparBuffer();
 
     printf(" Digite o nome da cidade: ");
@@ -246,7 +263,7 @@ int main() {
                 printf("|| Super Poder: %.f\n", resultadoComparacaoSuper.superCarta2); // Exibindo o super poder.
                 printf("--------------------------------------------\n");
 
-                printf(" Pressione ENTER para voltar ao menu..\n");
+                printf("\n Pressione ENTER para voltar ao menu..\n");
                 limparBuffer(); //limpa o buffer.
             break;
             
@@ -286,22 +303,77 @@ int main() {
                 compararAtributo(&carta1, &carta2, escolhaComparacao1);
                 printf("\n");
                 compararAtributo(&carta1, &carta2, escolhaComparacao2);
-
                 printf("\n");
-                printf(" Pressione ENTER para calcular..\n");
+
+                //soma e comparação dos atributos.
+                printf("\n Pressione ENTER para calcular..\n");
                 getchar();
+                system("clear"); //limpar teminal.
+                
+                struct ResultadoRodada resultadosDaRodada;
 
-                system("clear");
-                double somaComparacao = carta1.escolhaComparacao1;
+                resultadosDaRodada.somaCarta1 = 0.0f;
+                resultadosDaRodada.somaCarta2 = 0.0f;
 
+                //soma da carta 1.
+                resultadosDaRodada.somaCarta1 += obterValorAtributo(&carta1, escolhaComparacao1);
+                resultadosDaRodada.somaCarta1 += obterValorAtributo(&carta1, escolhaComparacao2);
 
+                //soma da carta 2.
+                resultadosDaRodada.somaCarta2 += obterValorAtributo(&carta2, escolhaComparacao1);
+                resultadosDaRodada.somaCarta2 += obterValorAtributo(&carta2, escolhaComparacao2);
+
+                printf("----------- Resultado Final ------------\n");
+                printf("\n Soma dos atributos de %s:\n %.2f\n", carta1.cidade, resultadosDaRodada.somaCarta1);
+                printf("\n Soma dos atributos de %s:\n %.2f\n", carta2.cidade, resultadosDaRodada.somaCarta2);
+                printf("--------------------------------------\n");
+
+                if (resultadosDaRodada.somaCarta1 > resultadosDaRodada.somaCarta2) {
+                    printf("\n %s venceu a rodada com a maior soma!\n", carta1.cidade);
+                } else if (resultadosDaRodada.somaCarta2 > resultadosDaRodada.somaCarta1) {
+                    printf("\n %s venceu a rodada com a maior soma!\n", carta2.cidade);
+                } else {
+                    printf("\n A rodada terminou em Empate!\n");
+                }
+                
                 printf("\n");
                 printf(" Pressione ENTER para voltar ao menu..\n");
                 limparBuffer();
             break;
 
             case 3:
+                printf("|| População: %s\n",
+                carta1.populacao > carta2.populacao ? "Carta 1 venceu!!" :
+                (carta2.populacao > carta1.populacao ? "Carta 2 venceu!!" : "Empate!"));
                 
+                printf("|| Área: %s\n",
+                carta1.area > carta2.area ? "Carta 1 venceu!!" : 
+                (carta2.area > carta1.area ? "Carta 2 venceu!!" : "Empate!"));
+                
+                printf("|| PIB: %s\n",
+                carta1.PIB > carta2.PIB ? "Carta 1 venceu!!" : 
+                (carta2.PIB > carta1.PIB ? "Carta 2 venceu!!" : "Empate!"));
+            
+                printf("|| Pontos Turisticos: %s\n",
+                carta1.pontosTuristicos > carta2.pontosTuristicos ? "Carta 1 venceu!!" : 
+                (carta2.pontosTuristicos > carta1.pontosTuristicos ? "Carta 2 venceu!!" : "Empate!"));
+                
+                // a carta com o menor valor vence
+                printf("|| Densidade Populacional: %s\n",
+                carta1.resultados.densidadePopulacional < carta2.resultados.densidadePopulacional ? "Carta 1 venceu!!" :
+                (carta2.resultados.densidadePopulacional < carta1.resultados.densidadePopulacional ? "Carta 2 venceu!!" : "Empate!"));
+                
+                printf("|| PIB per Capta: %s\n",
+                carta1.resultados.PIBperCapta > carta2.resultados.PIBperCapta ? "Carta 1 venceu!!" : 
+                (carta2.resultados.PIBperCapta > carta1.resultados.PIBperCapta ? "Carta 2 venceu!!" : "Empate!"));
+                
+                printf("|| Super Poder: %s\n",
+                resultadoComparacaoSuper.superCarta1 > resultadoComparacaoSuper.superCarta2 ? "Carta 1 venceu!!" : 
+                (resultadoComparacaoSuper.superCarta2 > resultadoComparacaoSuper.superCarta1 ? "Carta 2 venceu!!" : "Empate!"));
+
+                printf("\n");
+                printf(" Pressione ENTER para voltar ao menu..\n");
+                limparBuffer();
             break;
             
             default:
